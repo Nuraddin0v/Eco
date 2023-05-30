8418a6cf285ed1782347f930cb32fd70444d21e0
Jahongir <jahongirergawev2@gmail.com>
2023-05-30 14:15:43 +0500

    new change prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: false,
+  bracketSameLine: false,
+  trailingComma: "es5",
+  printWidth: 100,
+  tabWidth: 2,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+};
